eb0b107bc75cf84630046bd18e8dec7dee9053a9
mario.sigel <mario.sigel@sociovestix.com>
2021-04-22 15:30:22 +0200

    setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="my-utilities", # Replace with your own username
+    name="myutilities", # Replace with your own username
     version="0.0.1",
     author="Mario Sigel",
     author_email="mario.sigel@sociovestix.com",

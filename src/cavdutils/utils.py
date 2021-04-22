fec81db0c0610d6f1be89871a4c820743c8162be
mario.sigel <mario.sigel@sociovestix.com>
2021-04-22 16:41:35 +0200

    geonames search

--- src/cavdutils/utils.py
+++ src/cavdutils/utils.py
@@ -1,10 +1,12 @@
 import requests
+import pandas as pd
 
 
 GEONAMES_URL = 'http://api.geonames.org'
 
 
-def query_geonames(company_name, page_addresses):
+def query_geonames(company_name, addresses):
+    addresses = {}
     codes = ['MFG', 'FCL', 'TOWR']  # codes from https://www.geonames.org/export/codes.html
     for c in codes:
         res = requests.get(GEONAMES_URL + '/searchJSON?q={}&featureCode={}&username={}'.format(
@@ -16,4 +18,14 @@ def query_geonames(company_name, page_addresses):
         res = res.json()['geonames']
         res = {x['geonameId']: x for x in res}
 
-        page_addresses.update(res)
+        addresses.update(res)
+
+        addresses = {k: v for k, v in addresses.items() if any(
+            [company_name in (v.get(field).lower() or '') for field in ['name', 'toponymName']])
+                     }
+
+        keys = sorted(addresses[list(addresses.keys())[0]].keys())
+        addresses = pd.DataFrame([[addresses[_id].get(k) for k in keys]
+                                  for _id in addresses],
+                                 columns=keys)
+        return addresses

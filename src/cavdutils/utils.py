fca5579e4982a8292691c4128c4dc202f0401030
mario.sigel <mario.sigel@sociovestix.com>
2021-04-22 16:42:25 +0200

    geonames search

--- src/cavdutils/utils.py
+++ src/cavdutils/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 GEONAMES_URL = 'http://api.geonames.org'
 
 
-def query_geonames(company_name, addresses):
+def query_geonames(company_name):
     addresses = {}
     codes = ['MFG', 'FCL', 'TOWR']  # codes from https://www.geonames.org/export/codes.html
     for c in codes:
